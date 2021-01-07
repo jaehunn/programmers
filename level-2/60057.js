650ff3296c66b57621869635f2ab45bcc840cd94
bangjaehun <qkdwogns98@gmail.com>
2021-01-07 12:32:46 +0900

    ...

---
+++ level-2/60057.js
@@ -0,0 +1,37 @@
+function solution(s) {
+  if (s.length === 1) return 1;
+
+  let r = 1000;
+
+  let l = (s.length / 2) << 0;
+  while (l) {
+    let pre = s.substring(0, l);
+    let _s = [pre];
+    let _sI = 0;
+
+    // comp
+    let cnt = 1;
+    for (let i = l; i < s.length; i += l) {
+      let cur = s.substring(i, i + l); // (+rest string)
+
+      if (pre === cur) _s[_sI] = ++cnt + cur;
+      else {
+        _s[++_sI] = cur;
+
+        cnt = 1;
+      }
+
+      pre = cur;
+    }
+
+    // update
+    r = Math.min(
+      r,
+      _s.reduce((t, s) => (t += s.length), 0)
+    );
+
+    l -= 1;
+  }
+
+  return r;
+}
