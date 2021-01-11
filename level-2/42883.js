33e61665bf00a0bbdcfa965ad2b22eac788da030
bangjaehun <qkdwogns98@gmail.com>
2021-01-11 12:23:02 +0900

    ...

--- level-2/42883.js
+++ level-2/42883.js
@@ -1,38 +1,17 @@
-// wip
 function solution(number, k) {
-  let l = number.length;
-  let r = Array(l - k)
-    .fill()
-    .map((v, i) => l - i - 1)
-    .reverse();
+  let stk = [];
 
-  // number: [1 9 2 4], k = 2
-  // r     : [2, 3]
+  // number order
+  // k = remove number
+  for (let c of number) {
+    while (k && stk[stk.length - 1] < +c) {
+      stk.pop();
 
-  // 1 *9 <- 2
-  // 2 *4 <- 3
-  // 94
-  let e = -1;
-  for (let i = 0; i < r.length; i += 1) {
-    let mI = r[i];
-    let m = +number.charAt(mI);
-
-    for (let j = r[i] - 1; j > e; j -= 1) {
-      let n = +number.charAt(j);
-
-      // update
-      if (m <= n) {
-        mI = j;
-
-        m = +number.charAt(mI);
-      }
+      k -= 1;
     }
 
-    r[i] = mI;
-    e = r[i];
+    stk.push(+c);
   }
 
-  return r.map((idx) => number.charAt(idx)).join("");
+  return stk.join("");
 }
-
-console.log(solution("4177252841", 4));
