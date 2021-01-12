9469adf421014b69c9ba69b6fa5bf427d9105c2a
bangjaehun <qkdwogns98@gmail.com>
2021-01-12 13:56:12 +0900

    ...

---
+++ level-2/42960.js
@@ -0,0 +1,39 @@
+// wip
+function solution(name) {
+  const h = {
+    A: 0, // pass
+    B: 1,
+    C: 2,
+    D: 3,
+    E: 4,
+    F: 5,
+    G: 6,
+    H: 7,
+    I: 8,
+    J: 9,
+    K: 10,
+    L: 11,
+    M: 12,
+    N: 13,
+    O: 12,
+    P: 11,
+    Q: 10,
+    R: 9,
+    S: 8,
+    T: 7,
+    U: 6,
+    V: 5,
+    W: 4,
+    X: 3,
+    Y: 2,
+    Z: 1,
+  };
+
+  // wip
+  // 매번 가까운 A 를 찾기 -> Greedy
+}
+
+console.log(solution("JABAAZ")); // 14, 13
+// JABAAZ 14
+// JABAZ 14
+// JABZ 13
