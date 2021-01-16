d429883ae9486ca21ab43f2eba5716f81e3100cd
bangjaehun <qkdwogns98@gmail.com>
2021-01-16 13:53:42 +0900

    ...

--- level-2/42885.js
+++ level-2/42885.js
@@ -1,12 +1,29 @@
-// wip
-function solution(people, limit, l = people.length) {
-  // 57 = 28 + 29
-  // 58 = 28 + 28
-  let r = people.filter(
-    (v) => v <= (limit % 2 ? ((limit / 2) << 0) + 1 : (limit / 2) << 0)
-  ).length;
+// // wip
+// function solution(people, limit, l = people.length) {
+//   // 57 = 28 + 29
+//   // 58 = 28 + 28
+//   let r = people.filter(
+//     (v) => v <= (limit % 2 ? ((limit / 2) << 0) + 1 : (limit / 2) << 0)
+//   ).length;
 
-  return r % 2 ? l - r + ((r / 2) << 0) + 1 : (l - r + r / 2) << 0;
+//   return r % 2 ? l - r + ((r / 2) << 0) + 1 : (l - r + r / 2) << 0;
+// }
+
+function solution(people, limit) {
+  if (people.length === 1) return 1; // single
+
+  people.sort((a, b) => a - b);
+  let res = 0;
+
+  let l = 0;
+  let r = people.length - 1;
+
+  while (l <= r) {
+    if (people[l] + people[r] <= limit) l += 1;
+    r -= 1;
+
+    res += 1;
+  }
+
+  return res;
 }
-
-function _solution(people, limit, l = people.length) {}
---
+++ level-2/43165.js
@@ -0,0 +1,19 @@
+function solution(numbers, target) {
+  let res = 0; // scala value -> inner func
+  dfs(numbers, 0, target);
+
+  return res;
+
+  function dfs(numbers, cur, target) {
+    if (numbers.length === 0) {
+      if (cur === target) res += 1;
+
+      return;
+    }
+
+    let first = numbers[0];
+
+    dfs(numbers.slice(1), cur + first, target);
+    dfs(numbers.slice(1), cur - first, target);
+  }
+}
