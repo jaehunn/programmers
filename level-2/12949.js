7ac965366008329f74d93d909b0c8a9d78b32d7f
bangjaehun <qkdwogns98@gmail.com>
2021-01-23 13:10:10 +0900

    ...

--- level-2/12949.js
+++ level-2/12949.js
@@ -1,50 +1,15 @@
-// wip
 function solution(arr1, arr2) {
   let m = arr1.length;
   let n = arr2[0].length;
-  let r = []; // m x n
 
-  for (let i = 0; i < m; i += 1) {
-    let row = [];
+  // (m x p) x (p x n) = m x n
+  let res = Array(m)
+    .fill(0)
+    .map((r, i) =>
+      Array(n)
+        .fill(0)
+        .map((c, j) => arr1[i].reduce((a, c, k) => a + c * arr2[k][j], 0))
+    );
 
-    for (let j = 0; j < arr2.length; j += 1) {
-      let c = [];
-
-      for (let k = 0; k < n; k += 1) {
-        c.push(arr2[k][j]);
-      }
-
-      row.push(helper(arr1[i], c));
-    }
-
-    r.push(row);
-  }
-
-  return r;
+  return res;
 }
-
-function helper(m, n) {
-  let i = 0;
-  let r = 0;
-  while (i < m.length) {
-    r += m[i] * n[i];
-
-    i += 1;
-  }
-
-  return r;
-}
-
-console.log(
-  solution(
-    [
-      [1, 4],
-      [3, 2],
-      [4, 1],
-    ],
-    [
-      [3, 3],
-      [3, 3],
-    ]
-  )
-);
