28f73a76bdbdcaac70ce826f705dcade9f059e58
bangjaehun <qkdwogns98@gmail.com>
2021-01-27 21:06:53 +0900

    Nlcm

--- level-2/12953.js
+++ level-2/12953.js
@@ -1,4 +1,14 @@
-function solution(arr, l = arr.length) {
-  // coprime = [2, 3]
-  // wip
+function solution(arr) {
+  return arr.reduce((a, b) => lcm(a, b));
+}
+
+function lcm(a, b) {
+  return (a * b) / gcd(a, b);
+}
+
+// if a < b, swap
+function gcd(a, b) {
+  if (!b) return a;
+
+  return gcd(b, a % b);
 }
