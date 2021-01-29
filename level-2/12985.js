e6c712d462bddc7cb1fa5e32ff369fab2fa86a0d
bangjaehun <qkdwogns98@gmail.com>
2021-01-29 23:08:19 +0900

    ..

---
+++ level-2/12985.js
@@ -0,0 +1,21 @@
+// N = 8, A = 4, B = 7
+// 0 0 / 0 1 / 0 0 / 1 0 (1, 3)
+// 0 1 / 0 1 (0, 1)
+// 1 / 1 (0, 0) break
+function solution(n, a, b) {
+  // group
+  let i = ((a - 1) / 2) << 0;
+  let j = ((b - 1) / 2) << 0;
+  let r = 1;
+
+  while (i !== j) {
+    i = (i / 2) << 0;
+    j = (j / 2) << 0;
+
+    r += 1;
+  }
+
+  return r;
+}
+
+console.log(solution(8, 4, 7));
