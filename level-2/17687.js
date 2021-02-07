72ccc957f1fd4226109c73a7c844a615f2ea7219
bangjaehun <qkdwogns98@gmail.com>
2021-02-07 22:39:53 +0900

    level-2 cmplt

---
+++ level-2/17687.js
@@ -0,0 +1,23 @@
+function solution(n, t, m, p) {
+  let i = 0;
+  let s = "";
+  while (i <= t * m) {
+    s += i.toString(n);
+
+    i += 1;
+  }
+
+  let r = "";
+  let rI = p - 1;
+  while (r.length < t) {
+    let c = s.charAt(rI);
+
+    r += isNaN(+c) ? c.toUpperCase() : c;
+
+    rI += m; // m, 2m, 3m ...
+  }
+
+  return r;
+}
+
+solution(16, 16, 2, 1);
