532e0156b74522b7aa99836794f03beed7bf56e4
bangjaehun <qkdwogns98@gmail.com>
2021-02-12 13:41:00 +0900

    later on 42895

--- level-3/42895.js
+++ level-3/42895.js
@@ -16,7 +16,6 @@ function solution(N, number) {
     }
 
     let n = 0;
-
     for (let i = 1; i <= 8; i += 1) {
       n = n * 10 + N; // N NN NNN NNNN NNNNN...
 
@@ -28,4 +27,23 @@ function solution(N, number) {
   }
 }
 
-console.log(solution(5, 12));
+function solution(N, number) {
+  const cache = new Array(9).fill(0).map(() => new Set());
+
+  for (let i = 1; i < 9; i++) {
+    cache[i].add("1".repeat(i) * N);
+
+    for (let j = 1; j < i; j++) {
+      for (const arg1 of cache[j]) {
+        for (const arg2 of cache[i - j]) {
+          cache[i].add(arg1 + arg2);
+          cache[i].add(arg1 - arg2);
+          cache[i].add(arg1 * arg2);
+          cache[i].add((arg1 / arg2) >> 0);
+        }
+      }
+    }
+    if (cache[i].has(number)) return i;
+  }
+  return -1;
+}
