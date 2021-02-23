5b6ad2437a4ac0d0ca8d9dd275c403ef2274fd80
bangjaehun <qkdwogns98@gmail.com>
2021-02-23 10:15:02 +0900

    bfs

--- level-3/49189.js
+++ level-3/49189.js
@@ -25,13 +25,3 @@ function solution(n, E) {
 
   return D.filter((d) => d === Math.max(...D)).length;
 }
-
-solution(6, [
-  [3, 6],
-  [4, 3],
-  [3, 2],
-  [1, 3],
-  [1, 2],
-  [2, 4],
-  [5, 2],
-]);
