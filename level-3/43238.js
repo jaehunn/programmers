49884d6f97cdb240759aa8386254b7845d63fc93
bangjaehun <qkdwogns98@gmail.com>
2021-02-28 11:13:05 +0900

    binary search...

--- level-3/43238.js
+++ level-3/43238.js
@@ -1,27 +1,28 @@
 // wip
-function solution(n, times) {
+// 심사대의 처리는 독자적으로 이루어진다. 따라서 총 처리되는 사람 수 = (특정 시간 / 해당 심사대의 처리 시간) * 심사대 개수
+function solution(people, times) {
   times.sort((a, b) => a - b);
 
-  let l = 1;
-  let r = n * times[times.length - 1];
+  let minTime = 1;
+  let maxTime = people * times[times.length - 1];
 
-  let res = r;
-  while (l <= r) {
-    let m = ((l + r) / 2) << 0;
+  let targetTime = maxTime;
+  while (minTime <= maxTime) {
+    const avgTime = ((minTime + maxTime) / 2) << 0;
 
-    let c = 0;
+    let clearPeople = 0;
     times.forEach((time) => {
-      c += (m / time) << 0;
+      clearPeople += (avgTime / time) << 0;
 
-      if (c >= n) {
-        res = Math.min(m, r);
+      if (clearPeople >= people) {
+        targetTime = Math.min(targetTime, avgTime);
 
         return;
       }
     });
 
-    c >= n ? (r = m - 1) : (l = m + 1);
+    clearPeople >= people ? (maxTime = avgTime - 1) : (minTime = avgTime + 1);
   }
 
-  return res;
+  return targetTime;
 }
